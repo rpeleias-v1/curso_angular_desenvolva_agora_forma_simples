9327587561e9314134e8b9fe4f5603f4c2f19feb
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2017-05-13 14:54:30 -0300

    Seçã0 7 - aula 57: promises com http

--- secao_7/aula_57/gestao-vinhos/src/app/services/vinhos.service.ts
+++ secao_7/aula_57/gestao-vinhos/src/app/services/vinhos.service.ts
@@ -4,28 +4,22 @@ import { Vinho } from './../models/vinho';
 
 import { Http } from '@angular/http';
 
+import 'rxjs/add/operator/toPromise';
+
 @Injectable()
 export class VinhosService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   listar(): Promise<Array<Vinho>> {
-    let vinhos = new Array<Vinho>();
-    vinhos.push(this.criarVinho(1, 'Casillero Del Diablo', 'Cabernet Sauvignon', 'Tinto', 'Concha y Toro', 2010, 'Chile'));
-    vinhos.push(this.criarVinho(2, 'Casillero Del Diablo', 'Merlot', 'Tinto', 'Concha y Toro', 2015, 'Chile'));
-    return Promise.resolve(vinhos);
+    return this.http.get('api/vinhos')
+      .toPromise()
+      .then(response => response.json().data as Array<Vinho>)
+      .catch(this.tratarErro);
   }
 
-  private criarVinho(id: number, nome: string, uva: string, classificacao: string, fabricante: string, anoSafra:number, paisOrigem: string): Vinho {
-    let vinho:Vinho = new Vinho();
-    vinho.id = id;
-    vinho.nome = nome;
-    vinho.uva = uva;
-    vinho.classificacao = classificacao;
-    vinho.fabricante = fabricante;
-    vinho.anoSafra = anoSafra;
-    vinho.paisOrigem = paisOrigem;
-    return vinho;
-}
-
+  private tratarErro(erro:any): Promise<any> {
+    console.log(erro);
+    return Promise.reject(erro.message | erro);
+  }
 }
