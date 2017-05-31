4b6017e9cf4431e13f4d5091518a0fbf97abde86
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2017-05-30 21:30:17 -0300

    Aula 92 - seção 15

--- secao_15/aula_93/gestao-vinhos/src/app/components/page-container/page-container.component.ts
+++ secao_15/aula_93/gestao-vinhos/src/app/components/page-container/page-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'page-container',
@@ -8,15 +8,10 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class PageContainerComponent implements OnInit {
 
   @Input() titulo: string;
-  @Output() acaoVoltar: EventEmitter<string> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  voltar() {
-    this.acaoVoltar.emit();
   }
 
 }
---
+++ secao_15/aula_93/gestao-vinhos/src/app/pipes/filtro-tabela.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FiltroTabelaPipe } from './filtro-tabela.pipe';
+
+describe('FiltroTabelaPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FiltroTabelaPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
