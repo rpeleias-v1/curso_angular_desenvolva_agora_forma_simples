161110e9ef5be1475cccbe704d8d4c096ff75370
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2017-06-06 21:54:45 -0300

    Seção 16 - Aula 104

---
+++ secao_16/aula_104/gestao_vinhos/src/app/services/autenticacao.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/toPromise'
+
+@Injectable()
+export class AutenticacaoService {
+
+  private _usuarioLogado:boolean = false;
+  private token: string;
+
+  constructor(private http: Http) { }
+
+  login(login: string, senha: string): Promise<boolean> {
+    return this.http.post('/login', JSON.stringify({login: login, senha: senha}))
+      .toPromise()
+      .then((response: Response) => {
+          if(response.json() && response.json().token) {
+            this.token = response.json().token;
+            localStorage.setItem('usuarioSistema', JSON.stringify(response.json().usuario));  
+            return true;                  
+          } else {
+            localStorage.removeItem('usuarioSistema');
+            return false;
+          }
+      });    
+  }
+
+  logout(): void {
+    this.token = null;
+    localStorage.removeItem('usuarioSistema');
+  }
+
+  get usuarioLogado(): boolean {
+    return localStorage.getItem('usuarioSistema') !== null;
+  }
+
+}
